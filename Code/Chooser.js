52e473bf67d0e2fc239f1d2cbf08b75cd2c68ea2
apizzimenti <pizzimentianthony@gmail.com>
2015-08-05 18:25:24 -0500

    conditionals for button choice

--- Code/Chooser.js
+++ Code/Chooser.js
@@ -6,7 +6,23 @@
 $(document).ready(function () {
     "use strict";
     
-    var menu = document.getElementById("choice");
+    var menu = document.getElementById("choice"), $button = $("#apply"),
+        $input = document.getElementById("input");
     
-    
+    $button.click(function () {
+        var accept = $input.value;
+        
+        if (menu.value === "1") {
+            $("body").css("background-color", accept);
+        } else if (menu.value === "2") {
+            $("#bigger").css("background-color", accept);
+        } else if (menu.value === "3") {
+            $(".content-inner").css("font-family", accept);
+        } else if (menu.value === "4") {
+            $("#bigbio").css("font-family", accept);
+        } else if (menu.value === "5") {
+            alert("add drag and drop");
+        }
+        
+    });
 });
